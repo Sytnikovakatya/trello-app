75577c279fc9d9bfc509d7ff5b31fb43b18735f1
Kateryna Sytnikova <106179834+Sytnikovakatya@users.noreply.github.com>
2024-02-01 16:50:43 +0200

    client: *code style

--- client/src/services/reorder.service.ts
+++ client/src/services/reorder.service.ts
@@ -1,11 +1,11 @@
 import type { DraggableLocation } from '@hello-pangea/dnd';
-import { findIndex,  move,  insert, remove } from 'ramda';
+import { findIndex, move, insert, remove } from 'ramda';
 
 import { Card, List } from '../common/types';
 
 export const reorderService = {
   reorderLists<T>(items: T[], startIndex: number, endIndex: number): T[] {
-   return move(startIndex, endIndex, items);
+    return move(startIndex, endIndex, items);
   },
 
   reorderCards(
@@ -13,8 +13,12 @@ export const reorderService = {
     source: DraggableLocation,
     destination: DraggableLocation
   ): List[] {
-    const getSourceIndex = findIndex((list: List) => list.id === source.droppableId);
-    const getDestinationIndex = findIndex((list: List) => list.id === destination.droppableId);
+    const getSourceIndex = findIndex(
+      (list: List) => list.id === source.droppableId
+    );
+    const getDestinationIndex = findIndex(
+      (list: List) => list.id === destination.droppableId
+    );
 
     const current: Card[] = lists[getSourceIndex(lists)]?.cards || [];
     const next: Card[] = lists[getDestinationIndex(lists)]?.cards || [];
@@ -23,7 +27,11 @@ export const reorderService = {
     const isMovingInSameList = source.droppableId === destination.droppableId;
 
     if (isMovingInSameList) {
-      const reordered: Card[] = this.reorderLists(current, source.index, destination.index);
+      const reordered: Card[] = this.reorderLists(
+        current,
+        source.index,
+        destination.index
+      );
 
       return lists.map(list =>
         list.id === source.droppableId ? { ...list, cards: reordered } : list
@@ -50,10 +58,10 @@ export const reorderService = {
   },
 
   removeCardFromList(cards: Card[], index: number): Card[] {
-    return remove(index, 1, cards)
+    return remove(index, 1, cards);
   },
 
   addCardToList(cards: Card[], index: number, card: Card): Card[] {
-    return insert(index, card, cards)
+    return insert(index, card, cards);
   }
 };
